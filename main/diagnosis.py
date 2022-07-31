401210b4ae92d7ba45da7cc74e6492352ca31838
lt77777 <l.tsai@mail.utoronto.ca>
2022-07-31 14:41:11 -0600

    streamline data recording

--- main/diagnosis.py
+++ main/diagnosis.py
@@ -7,11 +7,15 @@ def diagnose_to_json(before, after):
     Diagnose the summary statistics before and after cover
     in cover/uncover eye test and save result to json file
     Parameters:
-        before: Dictionary of summary statistics before cover
-        after: Dictionary of summary statistics after cover
+        before: Path to JSON of summary statistics before cover
+        after: Path to JSON of summary statistics after cover
     Returns:
         diagnosis: String of diagnosis
     """
+    with open(before, "r") as f:
+        before = json.load(f)
+    with open(after, "r") as f:
+        after = json.load(f)
     diagnosis = diagnose(before, after)
     diagnosis_json = json.dumps({"diagnosis": diagnosis})
     if not os.path.exists("./results"):
@@ -33,6 +37,7 @@ def diagnose(before, after):
     TOLERANCE = 4 # mm
     N = 180 # number of samples
     T_CRIT = 1.9734 # critical t-value at 95% confidence with DOF=178
+
     expo_estotropia_test = (before["cornea10Dx"], after["cornea10Dx"], "Expotrophia", "Esotrophia") 
     hyper_hypotropia_test = (before["cornea10Dy"], after["cornea10Dy"], "Hypertropia", "Hypotrophia")
     test1_var =  cauchy_schwartz_variance(expo_estotropia_test[0]["std"], expo_estotropia_test[1]["std"], N)
