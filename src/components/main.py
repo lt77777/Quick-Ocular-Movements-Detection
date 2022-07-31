c256f7bd2f475d9fb7b4685eba615db4fc35c5db
lt77777 <l.tsai@mail.utoronto.ca>
2022-07-31 15:33:52 -0600

    add comments

--- src/components/main.py
+++ src/components/main.py
@@ -3,7 +3,11 @@ import time
 sys.path.insert(0, "./main")
 from webcam_measurement import *
 
+# 3 second delay
 time.sleep(3)
+# Before measurement
 webcam_measurement(3)
+# Cover
 time.sleep(1)
+# After measurement
 webcam_measurement(3)
